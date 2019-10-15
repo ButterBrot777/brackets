f51c43b0445d00bd63fb8e538d151a30d312c8ef
Pavel Karenda <korendos@gmail.com>
2019-10-15 20:53:20 +0300

    fix: the name of variable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = str.split('');
+    if(brackets.length % 2 !== 0){return false};
+    let openBrackets = [];
+    let closeBrackets = [];
+    for( let i =0; i < bracketsConfig.length; i++){
+        openBrackets.push(bracketsConfig[i][0]);
+        closeBrackets.push(bracketsConfig[i][1]);
+
+    }
+    console.log('brackets: ', brackets);
+    let stack = [];
+
+    for(let i=0; i < brackets.length; i++){
+        console.log(`brackets ${i} = `, brackets[i]);
+        if(openBrackets.includes(brackets[i])){
+            if (closeBrackets.indexOf(brackets[i]) === stack[stack.length - 1]) {
+                console.log('asdfasdfbjkasfjkhsghkj');
+                // console.log(`closeBrackets.indexOf(brackets[`,i,`]) = `, closeBrackets.indexOf(brackets[i]));
+                // console.log(`stack[stack.length - 1]`, stack[stack.length - 1])
+
+                stack.pop()
+                console.log('stack.pop: ', stack)
+            }
+            else stack.push(openBrackets.indexOf(brackets[i]));
+            console.log(`stack.push ${i}`, stack);
+        }
+
+        else if(closeBrackets.includes(brackets[i])){
+            if(stack[stack.length - 1] === closeBrackets.indexOf(brackets[i])){
+                stack.pop();
+                console.log(`stack.pop ${i}`, stack);
+
+
+            }
+
+            else {
+                console.log('last return', stack);
+                return false;
+            }
+            // console.log('for openBrackets: stack: ', stack);
+
+
+
+        }
+
+    }
+    // console.log('stack.length: ', stack.length);
+    console.log('length of stack: ', stack.length);
+    return stack.length === 0;
+
+
 }
+
+// check('[]()', [['(', ')'], ['[', ']']]);
