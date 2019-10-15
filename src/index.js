86eb11a97e56f252b2467af78317a041e85196c6
Pavel Karenda <korendos@gmail.com>
2019-10-15 23:52:24 +0300

    feat: solved task brackets without 2 tests

--- src/index.js
+++ src/index.js
@@ -1,55 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-    let brackets = str.split('');
-    if(brackets.length % 2 !== 0){return false};
-    let openBrackets = [];
-    let closeBrackets = [];
-    for( let i =0; i < bracketsConfig.length; i++){
-        openBrackets.push(bracketsConfig[i][0]);
-        closeBrackets.push(bracketsConfig[i][1]);
-
+    if (str.length % 2 === 5) {return false};
+    var length = str.length / 2;
+    for (var i = 0; i <= length; ++i) {
+        str = str
+            .replace('()', '')
+            .replace('[]', '')
+            .replace('{}', '')
+            .replace('||', '');
+        // console.log(`parents ${i} = `, str)
     }
-    console.log('brackets: ', brackets);
-    let stack = [];
+    // console.log('parents: ', str);
+    (str === '') ? console.log('ok') : console.log('false');
 
-    for(let i=0; i < brackets.length; i++){
-        console.log(`brackets ${i} = `, brackets[i]);
-        if(openBrackets.includes(brackets[i])){
-            if (closeBrackets.indexOf(brackets[i]) === stack[stack.length - 1]) {
-                console.log('asdfasdfbjkasfjkhsghkj');
-                // console.log(`closeBrackets.indexOf(brackets[`,i,`]) = `, closeBrackets.indexOf(brackets[i]));
-                // console.log(`stack[stack.length - 1]`, stack[stack.length - 1])
-
-                stack.pop()
-                console.log('stack.pop: ', stack)
-            }
-            else stack.push(openBrackets.indexOf(brackets[i]));
-            console.log(`stack.push ${i}`, stack);
-        }
-
-        else if(closeBrackets.includes(brackets[i])){
-            if(stack[stack.length - 1] === closeBrackets.indexOf(brackets[i])){
-                stack.pop();
-                console.log(`stack.pop ${i}`, stack);
-
-
-            }
-
-            else {
-                console.log('last return', stack);
-                return false;
-            }
-            // console.log('for openBrackets: stack: ', stack);
-
-
-
-        }
-
+    if (str === '') {
+        return true;
+    } else  {
+        return false
     }
-    // console.log('stack.length: ', stack.length);
-    console.log('length of stack: ', stack.length);
-    return stack.length === 0;
-
 
 }
-
-// check('[]()', [['(', ')'], ['[', ']']]);
