db8644723bdd1ab10d205cecbda552998cb4a29f
Pavel Karenda <korendos@gmail.com>
2020-03-05 23:29:00 +0300

    fix: solved 2 more tests, total 20 tests (100%)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-    if (str.length % 2 === 5) {return false};
-    var length = str.length / 2;
-    for (var i = 0; i <= length; ++i) {
-        str = str
-            .replace('()', '')
-            .replace('[]', '')
-            .replace('{}', '')
-            .replace('||', '');
-        // console.log(`parents ${i} = `, str)
+    let pairs = {};
+    bracketsConfig.forEach(elem => {
+        pairs[elem[0]] = elem[1];
+    }); // transform array into object
+    let result = [];
+    let brackets = str.split('');
+    for (let i = 0; i < str.length; i++) {
+        let lastBracket = result[result.length - 1]; // to control last symbol in stack
+        if( pairs[lastBracket] === brackets[i] ) {
+            result.pop();
+        } else {
+            result.push(brackets[i]);
+        }
     }
-    // console.log('parents: ', str);
-    (str === '') ? console.log('ok') : console.log('false');
-
-    if (str === '') {
-        return true;
-    } else  {
-        return false
-    }
-
+    return result.length === 0;
 }
